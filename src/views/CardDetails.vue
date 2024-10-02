c8fdf5f304f49dd50a61584bc5ac86afa87206c1
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-10-02 11:58:47 -0500

    Card Details Done

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,3 +1,9 @@
 <template>
-    Card Details
-</template>
+    {{ route.params.id }}
+  </template>
+  
+  <script setup>
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  </script>
